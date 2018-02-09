740052c197bdf62622b5eb097980d01e79338dc1
Szabloreki <dawid@surprise.design>
2018-02-09 18:15:03 +0100

    modal

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ import './styles.css'
 // }
 document.body.innerHTML = `
 <body>
+<div id="myModal" class="modal">
+<button id="myBtn">Open Modal</button>
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
     <div class="container">
         <div class="first-bar">
           <button class="up-setting">setting</button>
@@ -32,9 +39,6 @@ document.body.innerHTML = `
   </div>
 </body>
 `
-
-
-
 var typing = ''
 var sendInput = document.querySelector('.send-input')
 sendInput.addEventListener('keypress', (key) => {
@@ -45,26 +49,51 @@ sendInput.addEventListener('keypress', (key) => {
 }, false)
 
 var chatArea = document.querySelector('.chat-area');
- 
-const addMessages =  () => {
+
+const addMessages = () => {
     let message = document.createElement('span');
-    message.innerHTML = typing; 
+    message.innerHTML = typing;
     console.log('push force')
     message.classList = 'message';
     chatArea.appendChild(message);
     chatArea.scrollTop = chatArea.scrollHeight
- }
+}
 
 const upButton = document.querySelector('.up-btn')
-upButton.addEventListener('click', ()=> {
-  const container = document.querySelector('.container')
-  if (container.classList.contains('animate-up')) {
-    container.classList.remove('animate-up')
-  } else {
-    container.classList.add('animate-up')
-  }
+upButton.addEventListener('click', () => {
+    const container = document.querySelector('.container')
+    if (container.classList.contains('animate-up')) {
+        container.classList.remove('animate-up')
+    } else {
+        container.classList.add('animate-up')
+    }
 })
 
 
 
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
