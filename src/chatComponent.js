6b27a48b0c538557d7b9d799b6e9ae3b1b164c60
Szabloreki <dawid@surprise.design>
2018-02-10 03:42:10 +0100

    chatComponent improvment

--- src/chatComponent.js
+++ src/chatComponent.js
@@ -15,14 +15,13 @@ export const setFunctionsToChat = (con) => {
     const sendInput = document.querySelector('.send-input-sur-chat')
     const fileManager = document.querySelector('.file-manager');
 
-    console.log(this.sendInput)
     sendInput.addEventListener('keypress', (key) => {
         typing = sendInput.value;
         if (key.charCode == 13) {
+
             con.send(typing)
         }
     }, false)
-
 
     fileManager.addEventListener('change', (event) => {
         console.log(fileManager.value, 'even');
@@ -31,12 +30,13 @@ export const setFunctionsToChat = (con) => {
 
 export function addMessages(message) {
     const parent = document.querySelector('.chat-area-sur-chat');
+    const sendInput = document.querySelector('.send-input-sur-chat')
     const span = document.createElement('span');
+    sendInput.value = ''
     span.innerHTML = message.message;
     span.classList = 'message-sur-chat';
     parent.appendChild(span);
     parent.scrollTop = parent.scrollHeight
-
 }
 
 
